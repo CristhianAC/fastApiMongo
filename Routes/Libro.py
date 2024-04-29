dbb2361afae24407505bff2207d2826bc85a6e57
Cristhian Agamez <cristhian_agamez@outlook.com>
2024-04-29 00:12:06 -0500

    Refactor libro routes and delete Edicion model

--- Routes/Libro.py
+++ Routes/Libro.py
@@ -7,23 +7,26 @@ book = APIRouter()
 db = conn.investigacionMongo.libro
 
 
-@book.get("/libros", response_model=libro)
+@book.get("/libros")
 def get_books():
     return librosEntity(db.find({},{}))
-@book.post("/libros", response_model=libro)
+
+@book.post("/libros")
 def post_books(libro: libro):
     new_libro = dict(libro)
     id = db.insert_one(new_libro).inserted_id
     return str(id)
 
-@book.get("/libros/{titulo}", response_model=libro)
+@book.get("/libros/{titulo}")
 def get_book(titulo:str):
-    
     return libroEntity(db.find_one({"titulo":titulo},{}))
-@book.put("/libros/{titulo}", response_model=libro)
+
+@book.put("/libros/{titulo}")
 def update_book(titulo:str, libro:libro):
     db.find_one_and_update({"titulo":titulo},{"$set": dict(libro)})
-@book.delete("/libros/{titulo}", status_code=status.HTTP_204_NO_CONTENT)
+    
+    
+@book.delete("/libros/{titulo}")
 def delete_book(titulo:str):
     db.find_one_and_delete({"titulo":titulo})
     return Response(status_code=HTTP_204_NO_CONTENT)
--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from typing import Union
 from Routes.Libro import book
 app = FastAPI()
 
 app.include_router(book)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Koyeb"}
