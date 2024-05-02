98d95b18b3b7610584c1abdba49e487a94695369
Cristhian Agamez <cristhian_agamez@outlook.com>
2024-05-02 05:04:17 -0500

    Refactor libroEntity to copiaEntity in Schemas/Copia.py and add new routes for autor, autorea, copia, edicion, prestamo, and usuario

--- Schemas/Copia.py
+++ Schemas/Copia.py
@@ -1,10 +1,8 @@
-def libroEntity(item:dict)->dict:
+def copiaEntity(item:dict)->dict:
     return {
-        "id": str(item['_id']), # Convertir el ObjectId de mongo en un string para que pueda ser serializado a JSON
-        "titulo": item['titulo'],
-        "autor": item['autor'],
-        "editorial": item['editorial'],
-        
+        "numero": item['numero'], # Convertir el ObjectId de mongo en un string para que pueda ser serializado a JSON
+    
+       
     }
-def librosEntity(entity)->dict:
-    return [libroEntity(item) for item in entity]
+def copiasEntity(entity)->dict:
+    return [copiaEntity(item) for item in entity]
--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from typing import Union
 from Routes.Libro import book
+from Routes.Autorea import autorea
+from Routes.Copia import copia
+from Routes.Prestamo import prestamo
+from Routes.Usuario import user
+from Routes.Edicion import edicion
+from Routes.Autor import author
 app = FastAPI()
 
 app.include_router(book)
+app.include_router(autorea)
+app.include_router(copia)
+app.include_router(prestamo)
+app.include_router(user)
+app.include_router(edicion)
+app.include_router(author)
+
 
 @app.get("/")
 def read_root():
